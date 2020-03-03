30fb1802ed5a3de89ca688f81fb4570436077406
Xuanken Tay <Xuanken.Tay@quantium.com.au>
2020-03-03 15:55:52 +1100

    update pascal

--- coursera_functional_programming_principles_scala/recfun/src/main/scala/recfun/RecFun.scala
+++ coursera_functional_programming_principles_scala/recfun/src/main/scala/recfun/RecFun.scala
@@ -15,7 +15,8 @@ object RecFun extends RecFunInterface {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0 || c == r) 1
+    if (c < 0 || r < 0 || c > r) throw new IndexOutOfBoundsException()
+    else if (c == 0 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
